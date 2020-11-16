957e0879c3f1f4722d5ed7c4f5bc31ae61456060
sakib2439 <sakib2439@gmail.com>
2020-11-16 12:09:42 +0600

    helper edit

--- app/helper.php
+++ app/helper.php
@@ -23,15 +23,15 @@ function options(){
 }
 
 function shuffle_assoc($list) {
-  if (!is_array($list)) return $list;
+    if (!is_array($list)) return $list;
 
-  $keys = array_keys($list);
-  shuffle($keys);
-  $random = array();
-  foreach ($keys as $key)
-    $random[$key] = $list[$key];
+    $keys = array_keys($list);
+    shuffle($keys);
+    $random = array();
+    foreach ($keys as $key)
+      $random[$key] = $list[$key];
 
-  return $random;
+    return $random;
 }
 
 function sendExamPaperMail($view,$data)
